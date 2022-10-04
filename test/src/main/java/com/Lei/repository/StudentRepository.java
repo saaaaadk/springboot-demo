25e80834e770ca0d604ac05675d5eb84068a384c
Leilei <2960047723@qq.com>
2022-10-04 21:43:53 +0800

    221004

--- test/src/main/java/com/Lei/repository/StudentRepository.java
+++ test/src/main/java/com/Lei/repository/StudentRepository.java
@@ -2,16 +2,13 @@ package com.Lei.repository;
 
 import com.Lei.entity.Student;
 
-import java.util.List;
+import java.util.Collection;
 
 public interface StudentRepository {
-    public List<Student> findAll();
-
+    public Collection<Student> findAll();
     public Student findById(long id);
-
     public void save(Student student);
-
     public void update(Student student);
-
     public void deleteById(long id);
+
 }
