56ea9774186bd65117c27931cbf6a46d98e56b54
saaaaadk <2960047723@qq.com>
2022-10-15 10:23:53 +0800

    20221015

--- ssm-vue/vue-project/src/api/index.js
+++ ssm-vue/vue-project/src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // 创建axios实例
 const request = axios.create({
     // axios中请求配置有baseURL选项，表示请求URL公共部分
-    baseURL: 'http://localhost:9090',
+    baseURL: '/api/admin',
     // 超时
     timeout: 10000,
     // 设置Content-Type，规定了前后端的交互使用json
--- ssm-vue/vue-project/vite.config.js
+++ ssm-vue/vue-project/vite.config.js
@@ -5,10 +5,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-        port: 8080,
+        port: 7070,
         proxy: {
             '/api': {
-                target: 'http://localhost:9090/admin',
+                target: 'http://localhost:9090',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, '')
             },
